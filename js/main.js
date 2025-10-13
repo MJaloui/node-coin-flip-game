da965a97bb706b002b8ad1e69bffae43e879561c
MJaloui <alouidor.m@gmail.com>
2025-10-13 14:01:01 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,48 @@
+document.querySelector('#flipButton').addEventListener('click', flipButton)
+
+function flipButton() {
+
+
+
+  fetch(`/coinFlipApi`)
+    .then(response => response.text())
+    .then((data) => {
+      console.log(data);
+
+      const result = document.querySelector("#coinResult").textContent = data;
+
+    });
+
+}
+
+// document.getElementById("clickMe").onclick = makeReq;
+//
+// function makeReq(){
+//
+//   var userName = document.getElementById("userName").value;
+//
+//   var request = new XMLHttpRequest();
+//   request.open('GET', '/api?student='+userName, true);
+//
+//   request.onload = function() {
+//       console.log("works")
+//       if (request.status >= 200 && request.status < 400) {
+//         // Success!
+//         var data = JSON.parse(request.responseText);
+//         console.log(data)
+//         document.getElementById("personName").innerHTML = data.name
+//         document.getElementById("personStatus").innerHTML = data.status
+//         document.getElementById("personOccupation").innerHTML = data.currentOccupation
+//
+//       } else {
+//         // We reached our target server, but it returned an error
+//
+//       }
+//     };
+//
+//     request.onerror = function() {
+//       // There was a connection error of some sort
+//     };
+//
+//     request.send();
+// }
